deb07825e92f2b2e30361a5c0f2e6c4e9e9a5897
Pasindu Madusanka <116091367+pasindu526@users.noreply.github.com>
2025-11-07 08:28:55 +0530

    Completed Parlours page (Oct 29, 2025) (#7)
    * routing update
    
    * Updated Contact Us Page
    
    * Finalized The Contact Us Page
    
    * Final Update of Contact Us Page
    
    * Updated contact us page
    
    * Upadated the Gallery Page
    
    * Updated the Gallery Page
    
    * Updated the Gallery Page
    
    * Updated gallery page
    
    * Updated
    
    * Developed the Public Figures Page
    
    * Updated the Pubic Figures Page - v2 (#1)
    
    * Update the Card Section
    
    * Update the Public Figure Page
    
    * Completed the Public Figures Page
    
    * Update the Footer and the Navbar
    
    * Updated the Footer
    
    * Updated the Navbar
    
    * Updated the Public Figures Page
    
    * Updated
    
    * Updated Gallery Page, Footer and Navbar
    
    * Updated the Footer
    
    * Updated the Gallery Page Image Grid
    
    * Updated the Navbar
    
    * Updated the Public Figures Page Card Section
    
    * Updated all updates
    
    * Finalized all the changes up to now  (#2)
    
    * Update the Card Section
    
    * Update the Public Figure Page
    
    * Completed the Public Figures Page
    
    * Update the Footer and the Navbar
    
    * Updated the Footer
    
    * Updated the Navbar
    
    * Updated the Public Figures Page
    
    * Updated
    
    * Updated Gallery Page, Footer and Navbar
    
    * Updated the Footer
    
    * Updated the Gallery Page Image Grid
    
    * Updated the Navbar
    
    * Updated the Public Figures Page Card Section
    
    * Updated all updates
    
    * Updated Footer and Navbar
    
    * Updated Gallery Page
    
    * Updated
    
    * Updated the Footer
    
    * Updated About Us page
    
    * Update About Us Page
    
    * Updated the modifications
    
    * Updated Gallery
    
    * Updated
    
    * Updated all changes
    
    * Updated
    
    * Updated  font applying, MP logos, and existing components with changing styles.
    
    * Created Home page Key Services Section
    
    * updated container width
    
    * updated font
    
    * Created Home Page with Components (#3)
    
    * Updated  font applying, MP logos, and existing components with changing styles.
    
    * Created Home page Key Services Section
    
    * Created package section
    
    * Modified consultation section
    
    * Completed testimonials section
    
    * Completed Package Category section
    
    * Completed Home Page and Created Pre Arrangement Packages Page with updating Routes.
    
    * Completed Home Page Development (2025-10-23) (#4)
    
    * Updated  font applying, MP logos, and existing components with changing styles.
    
    * Created Home page Key Services Section
    
    * updated container width
    
    * updated font
    
    * Created package section
    
    * Modified consultation section
    
    * Completed testimonials section
    
    * Completed Package Category section
    
    * Completed Home Page and Created Pre Arrangement Packages Page with updating Routes.
    
    * modified home page key services section
    
    * updated hero overlay
    
    * Added asstes for both Packages & Pre Arrangement Packages pages
    
    * Created both Packages & Pre Arrangement Packages pages with components
    
    * Updated home page Package section
    
    * Installed Shadcn UI for project and, installed Select component.
    
    * Override shadcn class
    
    * Completed both Packages & Pre-Arrangement Packages pages with components (without package details pages) (#5)
    
    * modified home page key services section
    
    * updated hero overlay
    
    * Added asstes for both Packages & Pre Arrangement Packages pages
    
    * Created both Packages & Pre Arrangement Packages pages with components
    
    * Updated home page Package section
    
    * Installed Shadcn UI for project and, installed Select component.
    
    * Override shadcn class
    
    * Completed both Packages & Pre Arrangement Packages pages with package details popup. (with adding sample images)
    
    * Completed Parlours page
    
    * Install MUI
    
    ---------
    
    Co-authored-by: HirumiYeshani <2005yesha@gmail.com>

---
+++ src/components/parlours/HeroSection.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import HeroImg from "../../assets/img/parlourHero.svg";
+
+const HeroSection: React.FC = () => {
+  return (
+    <div>
+      <div className="relative  h-[300px] md:h-[400px] lg:h-[500px]">
+        <div className="absolute top-0 left-0 w-full"></div>
+        <img
+          src={HeroImg}
+          alt="Contact Hero"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black via-black/60 to-transparent">
+          <div className="w-11/12 md:w-5/6 mx-auto py-4 space-y-2">
+            <h1 className="text-2xl md:text-4xl lg:text-5xl leading-[34px] lg:leading-[62px] font-bold font-belda text-white text-center lg:text-left">
+              Serene & Supportive{" "}
+              <span className=" text-primary font-bold font-belda">
+                Funeral Parlours
+              </span>
+            </h1>
+
+            <p className="text-sm lg:text-xl leading-[20px]  lg:leading-[24px] text-white pb-2 sm:pb-4 text-center lg:text-left">
+              Providing peaceful spaces for families to gather and remember.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
