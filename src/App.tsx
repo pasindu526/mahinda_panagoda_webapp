2c48c2eef2e98974527d20422f42fd2a710b79c4
pasindu526 <pasindumadusanka526@gmail.com>
2025-11-07 23:12:04 +0530

    Updated Routs

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,15 @@ import Home from "./pages/Home";
 import Guidlines from "./pages/Guidlines";
 import PreArrangements from "./pages/PreArrangements";
 import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Home />} />
         <Route path="/contactus" element={<ContactUs />} />
         <Route path="/aboutus" element={<AboutUs />} />
